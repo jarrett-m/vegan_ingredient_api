1c82bd6eb6d89ff2fd408a558c14d6cdf6e21c5c
Jarrett <jarrettminton00@gmail.com>
2022-04-12 16:19:41 -0600

    Convert JSON to SQL3
    Faster performance loading from sql3 than from JSON!

--- convert_data_to_better_data.py
+++ convert_data_to_better_data.py
@@ -1,4 +1,6 @@
 import json
+import sqlite3
+
 
 data = {}
 
@@ -7,7 +9,24 @@ with open('FoodData_Central_branded_food_json_2021-10-28.json', 'rb') as foods:
     data = json.load(foods)
 print("done!")
 
-food = {}
+foods = {}
+
+'''
+i = 0
+for item in data["BrandedFoods"]:
+    if i % 10000 == 0:
+        print(f'item {i} complete')
+    i += 1
+
+    foods[item["gtinUpc"]] = item
+'''
+
+
+#print("dumping json...")
+#with open("refined_foods.json", "w") as outfile:
+#    json.dump(food, outfile)
+#print("done!")
+
 
 i = 0
 for item in data["BrandedFoods"]:
@@ -15,9 +34,40 @@ for item in data["BrandedFoods"]:
         print(f'item {i} complete')
     i += 1
 
-    food[item["gtinUpc"]] = item
+    newUPC = ''
+    for num in item["gtinUpc"]:
+        if num.isdigit():
+            newUPC += num
+        
 
-print("dumping json...")
-with open("refined_foods.json", "w") as outfile:
-    json.dump(food, outfile)
-print("done!")
+    foods[newUPC] = {
+        "gtinUpc": newUPC,
+        "description": item["description"],
+        "ingredients": item["ingredients"],
+        "brandOwner": item["brandOwner"]
+    }
+
+
+
+connection_obj = sqlite3.connect('foods.db')
+cursor_obj = connection_obj.cursor()
+cursor_obj.execute("DROP TABLE IF EXISTS FOODS")
+table = " CREATE TABLE FOODS (gtinUpc VARCHAR PRIMARY KEY, description VARCHAR(500) NOT NULL, ingredients VARCHAR(50000) NOT NULL, brandOwner VARCHAR(500)); "
+cursor_obj.execute(table)
+
+print("Table is Ready")
+
+i = 0
+for key in foods.keys():
+    if i % 10000 == 0:
+        print(f'item {i} complete')
+    i += 1
+
+    command = "INSERT or REPLACE INTO FOODS VALUES (?,?,?,?)"
+    if key != '':
+        cursor_obj.execute(command, [foods[key]["gtinUpc"], foods[key]["description"], foods[key]["ingredients"], foods[key]["brandOwner"]])
+
+    #print(int(foods[key]["gtinUpc"]), foods[key]["description"], foods[key]["ingredients"], foods[key]["brandOwner"])
+    #cursor_obj.execute(command, [int(foods[key]["gtinUpc"]), foods[key]["description"], foods[key]["ingredients"], foods[key]["brandOwner"]])
+connection_obj.commit()
+connection_obj.close()
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from select import select
+from click import command
 from fastapi import FastAPI, status, Response
 import vegan_checker as vc
 import json, re
+import aiosqlite
+
 
 app = FastAPI()
 
+'''
 print("loading json...")
 with open('refined_foods.json', 'rb') as foods:
     data = json.load(foods)
 print("done!")
+'''
+
 
 
 @app.get("/")
@@ -17,8 +24,12 @@ async def root():
 
 @app.get("/food/{gtinUpc}")
 async def get_ingd(gtinUpc, response: Response):
+    connection_obj =  await aiosqlite.connect('foods.db')
+    cursor_obj = await connection_obj.cursor()
     try:
-        result = data[gtinUpc]["ingredients"]
+        await cursor_obj.execute("SELECT ingredients FROM foods WHERE gtinUpc=?", (gtinUpc,))
+        result = await cursor_obj.fetchall()
+        result = result[0]
     except KeyError:
         result = {"err": f"No food with gtinUpc: {gtinUpc}"}
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -27,17 +38,26 @@ async def get_ingd(gtinUpc, response: Response):
 
 @app.get("/vegan_ingredents/{gtinUpc}")
 async def get_ingd(gtinUpc, response: Response):
-    ingreds = await ing_spliter(data[gtinUpc]["ingredients"])
+
     try:
+        connection_obj =  await aiosqlite.connect('foods.db')
+        cursor_obj = await connection_obj.cursor()
+        await cursor_obj.execute("SELECT ingredients, description FROM foods WHERE gtinUpc=?", (gtinUpc,))
+        data = await cursor_obj.fetchall()
+
+        ingreds = await ing_spliter(data[0][0])
+        name = data[0][1]
+
         not_vegan = await vc.contains_nonvegan(ingreds)
         maybe_vegan = await vc.contains_maybevegan(ingreds)
-        vegan = [i for i in ingreds if i not in not_vegan and i not in maybe_vegan]
+        vegan =  [i for i in ingreds if i not in not_vegan and i not in maybe_vegan]
         result = {  
-                    "name" : data[gtinUpc]["description"],
+                    "name" : name,
                     "not vegan" : not_vegan,
                     "maybe vegan" : maybe_vegan,
                     "vegan" : vegan
                 }
+
     except KeyError:
         result = {"err": f"No food with gtinUpc: {gtinUpc}"}
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -45,4 +65,5 @@ async def get_ingd(gtinUpc, response: Response):
 
 
 async def ing_spliter(ingredients):
+    ingredients = ingredients.lower()
     return re.split(r',\s*(?![^()]*\))', str(ingredients))
